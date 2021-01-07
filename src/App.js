15e159491d583a1271af74b07ce8acb445d9b197
Alyssa Karakula <alyssakarakula@Alyssas-MacBook-Air.local>
2021-01-06 19:25:52 -0500

    adds header and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <p>Project 3
-     </p>
+      <Header />
+        <p>Project 3</p>
+      <Footer />
     </div>
   );
 }
