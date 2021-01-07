9a29c2ca669e39b221af07ce6b0dde608852c7f0
Alyssa Karakula <alyssakarakula@Alyssas-MacBook-Air.local>
2021-01-06 20:21:09 -0500

    adds basic CSR

--- src/App.js
+++ src/App.js
@@ -2,12 +2,38 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+
+import { Switch, Route } from 'react-router-dom';
+// create 4 page components , login signup dashboard home
+// define as function and export them 
+// return a div with text content 
+//import them into APP.js
+// import switch from React Router then define various routes
 
 function App() {
   return (
     <div className="App">
       <Header />
-        <p>Project 3</p>
+        <main>
+          <Switch>
+            <Route exact path="/" render={props =>
+              <Home />
+            } />
+            <Route exact path="/dashboard" render={props =>
+              <Dashboard />
+            } />
+            <Route exact path="/signup" render={props =>
+              <Signup />
+            } />
+            <Route exact path="/login" render={props =>
+              <Login />
+            } />
+        </Switch>
+        </main>
       <Footer />
     </div>
   );
